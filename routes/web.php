00fca89c9a0dcff08dfe79ca3c1c456d54d0e943
antheiz <salupacode@gmail.com>
2021-05-20 11:49:40 +0900

    add folder per fitur

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('index');
-});
-
 Route::get('/signin', function () {
     return view('signin');
 });
